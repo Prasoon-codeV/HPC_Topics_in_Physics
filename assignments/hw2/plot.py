f96d9fabbc75893008f5ffced66cfaebe1ee08fb
Prasoon Vishwakarma <pxv220016@g2-l-02.localdomain>
2026-02-16 19:10:52 -0600

    Initial commit: empty solver module

--- assignments/hw2/plot.py
+++ assignments/hw2/plot.py
@@ -10,7 +10,7 @@ warnings.filterwarnings('ignore')
 file_name_1 = 'reion_history_Thesan1.dat'
 file_name_2 = 'sfrd_Thesan1.dat'
 
-data1 = pd.read_csv(file_name_1, delim_whitespace=True, skiprows=1, names=['z', 'x_HI', 'x_HeI', 'x_HeII'])
+data1 = pd.read_csv(file_name_1, sep=r"\s+", skiprows=1, names=['z', 'x_HI', 'x_HeI', 'x_HeII'])
 z = data1['z']
 x_HI = data1['x_HI']
 
