39be33d8beea637979a9c17905bca89e3e413c52
Prasoon Vishwakarma <pxv220016@g2-l-02.localdomain>
2026-02-19 11:55:49 -0600

    Adding tests in test_simple and new README.md

--- quantum-eigen/tests/test_simple.py
+++ quantum-eigen/tests/test_simple.py
@@ -8,3 +8,12 @@ def test_small_grid():
     # Basic check: eigenvalues should be ascending
     assert np.all(np.diff(vals) >= 0), "Eigenvalues are not sorted"
     print('--- Run Successful for test/tests.py---')
+
+
+def test_n_eigs():
+    vals, _ = solve_eigen(N=2, potential='well', n_eigs=5)
+    
+    # Basic check: number of eigenvalues shouldn't exceed N**N
+    assert len(vals) == 4, "Upper limit of size of Eigenvalues"
+    print('--- Run Successful for test/tests.py---')
+
